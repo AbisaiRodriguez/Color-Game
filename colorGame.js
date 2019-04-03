5755fd11db4d893b7a5e3a351982414735d040bb
Abisai Rodriguez <abisai.rodriguez13@gmail.com>
2019-04-02 21:21:21 -0700

    added js functionality

--- colorGame.js
+++ colorGame.js
@@ -1,14 +1,33 @@
 var colors = [
-    "rgb(255, 0, 0",
-    "rgb(0, 255, 0",
-    "rgb(0, 0, 255",
-    "rgb(150, 150, 150",
-    "rgb(150, 24, 200",
-    "rgb(255, 255, 3"
+    "rgb(255, 0, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 0, 255)",
+    "rgb(150, 150, 150)",
+    "rgb(150, 24, 200)",
+    "rgb(255, 255, 3)"
 ];
-
 var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
+colorDisplay.textContent = pickedColor;
+
 for(var i = 0; i < squares.length; i++){
+    //add initial colors to squares
     squares[i].style.backgroundColor = colors[i];
+
+    //add click listeners to squares
+    squares[i].addEventListener("click", function(){
+    //grab color of clicked square
+    var clickedColor = this.style.backgroundColor;
+    //compare color to pickedColor
+    if( clickedColor === pickedColor){
+        alert("Correct");
+    }
+    else{
+        alert("Wrong");
+        // this.style.backgroundColor = "#232323";
+    }
+
+    });
 }
 
