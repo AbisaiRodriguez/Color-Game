d02615d703f899f2ae230bf0a419d0388c8e82b0
Abisai Rodriguez <abisai.rodriguez13@gmail.com>
2019-04-02 22:04:27 -0700

    function changeColor

--- colorGame.js
+++ colorGame.js
@@ -9,25 +9,33 @@ var colors = [
 var squares = document.querySelectorAll(".square");
 var pickedColor = colors[3];
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.getElementById("message");
+
 colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i < squares.length; i++){
     //add initial colors to squares
     squares[i].style.backgroundColor = colors[i];
-
     //add click listeners to squares
     squares[i].addEventListener("click", function(){
     //grab color of clicked square
-    var clickedColor = this.style.backgroundColor;
+        var clickedColor = this.style.backgroundColor;
     //compare color to pickedColor
-    if( clickedColor === pickedColor){
-        alert("Correct");
-    }
-    else{
-        alert("Wrong");
-        // this.style.backgroundColor = "#232323";
-    }
-
+        if( clickedColor === pickedColor){
+        messageDisplay.textContent = "Correct";
+        changeColor(clickedColor);
+        }
+        else{
+        this.style.backgroundColor = "#232323";
+        messageDisplay.textContent = "Try Again";
+        }
     });
 }
 
+function changeColor(color){
+//loop through all squares
+    for(var i = 0; i < squares.length; i++){
+//change each color to match given color
+    squares[i].style.backgroundColor = color;
+    }
+}
